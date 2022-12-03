ff4f33e094ead1e729ab6143a96cefadc6e992be
Fredrik Andersson <git@git.glyph.se>
2022-12-03 06:31:42 +0100

    2022/Day03

--- Year2022/Day03/Solver.cs
+++ Year2022/Day03/Solver.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.Year2022.Day03
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Year2022.Day03
 {
     internal class Solver : ISolver
     {
@@ -6,16 +8,58 @@
         {
             await Task.Yield();
 
-            return "".ToString();
+            long score = 0;
+
+            foreach (string rucksack in Helpers.AsLines(input))
+            {
+                string part1 = rucksack.Substring(0, rucksack.Length / 2);
+                string part2 = rucksack.Substring(rucksack.Length / 2);
+
+                var result = part1.Intersect(part2);
+                char common = result.First();
+
+                if (common == char.ToLower(common))
+                {
+                    score += (common - 'a') + 1;
+                }
+                if (common == char.ToUpper(common))
+                {
+                    score += (common - 'A') + 27;
+                }
+            }
+
+            return score.ToString();
         }
 
         public async Task<string> PartTwo(string input)
         {
             await Task.Yield();
 
-            await Task.Yield();
+            long score = 0;
 
-            return "".ToString();
+            var rucksacks = Helpers.AsLines(input).ToArray();
+
+            for (int i = 0; i < rucksacks.Length; i = i + 3)
+            {
+                string part1 = rucksacks[i];
+                string part2 = rucksacks[i + 1];
+                string part3 = rucksacks[i + 2];
+
+                var result = part1.Intersect(part2).Intersect(part3);
+
+                char common = result.First();
+
+                if (common == char.ToLower(common))
+                {
+                    score += (common - 'a') + 1;
+                }
+                if (common == char.ToUpper(common))
+                {
+                    score += (common - 'A') + 27;
+                }
+            }
+
+            return score.ToString();
         }
     }
 }
