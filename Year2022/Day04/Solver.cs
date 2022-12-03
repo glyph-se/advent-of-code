fc84ea53f4ea6497bb428775b78ff3e10ea5be24
Fredrik Andersson <git@git.glyph.se>
2022-12-03 06:36:41 +0100

    Add skeleton for Day04

---
+++ Year2022/Day04/Solver.cs
@@ -0,0 +1,19 @@
+﻿namespace AdventOfCode.Year2022.Day04
+{
+    internal class Solver : ISolver
+    {
+        public async Task<string> PartOne(string input)
+        {
+            await Task.Yield();
+
+            return "".ToString();
+        }
+
+        public async Task<string> PartTwo(string input)
+        {
+            await Task.Yield();
+
+            return "".ToString();
+        }
+    }
+}
