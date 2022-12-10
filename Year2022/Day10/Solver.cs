73047d8c210ff65f01699bb96120d434fce92a7f
Fredrik Andersson <git@git.glyph.se>
2022-12-10 07:03:03 +0100

    Some code cleanup

--- Year2022/Day10/Solver.cs
+++ Year2022/Day10/Solver.cs
@@ -10,24 +10,17 @@ namespace AdventOfCode.Year2022.Day10
 
             int xValue = 1;
             int sumSignal = 0;
+            int cycle = 0;
 
             var lines = StringParsing.AsLines(input).ToList();
 
-            for (int cycle = 0; cycle < 1000;)
+            foreach (string command in lines)
             {
-                if (lines.Count == 0)
-                {
-                    break;
-                }
-
-                var command = lines[0];
-                lines.RemoveAt(0);
-
                 if (command == "noop")
                 {
                     cycle++;
 
-                    if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220)
+                    if (cycle % 40 == 20)
                     {
                         sumSignal += xValue * cycle;
                     }
@@ -35,22 +28,22 @@ namespace AdventOfCode.Year2022.Day10
                 else
                 {
                     var split = command.Split(' ');
-
-                    cycle++;
-
-                    if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220)
-                    {
-                        sumSignal += xValue * cycle;
-                    }
-
-                    cycle++;
-
-                    if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220)
-                    {
-                        sumSignal += xValue * cycle;
-                    }
-
                     int change = int.Parse(split[1]);
+
+                    cycle++;
+
+                    if (cycle % 40 == 20)
+                    {
+                        sumSignal += xValue * cycle;
+                    }
+
+                    cycle++;
+
+                    if (cycle % 40 == 20)
+                    {
+                        sumSignal += xValue * cycle;
+                    }
+
                     xValue += change;
                 }
             }
@@ -67,25 +60,17 @@ namespace AdventOfCode.Year2022.Day10
             result += "\n";
 
             int xValue = 1;
-            int sumSignal = 0;
+            int cycle = 0;
 
             var lines = StringParsing.AsLines(input).ToList();
 
-            for (int cycle = 0; cycle < 1000;)
+            foreach (string command in lines)
             {
-                if (lines.Count == 0)
-                {
-                    break;
-                }
-
-                var command = lines[0];
-                lines.RemoveAt(0);
-
                 if (command == "noop")
                 {
                     cycle++;
 
-                    if (xValue == ((cycle - 1) % 40) || xValue - 1 == ((cycle - 1) % 40) || xValue + 1 == ((cycle - 1) % 40))
+                    if (Math.Abs(((cycle - 1) % 40) - xValue) <= 1)
                     {
                         result += "#";
                     }
@@ -102,40 +87,40 @@ namespace AdventOfCode.Year2022.Day10
                 else
                 {
                     var split = command.Split(' ');
-
-                    cycle++;
-
-                    if (xValue == ((cycle - 1) % 40) || xValue - 1 == ((cycle - 1) % 40) || xValue + 1 == ((cycle - 1) % 40))
-                    {
-                        result += "#";
-                    }
-                    else
-                    {
-                        result += ".";
-                    }
-
-                    if (cycle % 40 == 0)
-                    {
-                        result += "\n";
-                    }
-
-                    cycle++;
-
-                    if (xValue == ((cycle - 1) % 40) || xValue - 1 == ((cycle - 1) % 40) || xValue + 1 == ((cycle - 1) % 40))
-                    {
-                        result += "#";
-                    }
-                    else
-                    {
-                        result += ".";
-                    }
-
-                    if (cycle % 40 == 0)
-                    {
-                        result += "\n";
-                    }
-
                     int change = int.Parse(split[1]);
+
+                    cycle++;
+
+                    if (Math.Abs(((cycle - 1) % 40) - xValue) <= 1)
+                    {
+                        result += "#";
+                    }
+                    else
+                    {
+                        result += ".";
+                    }
+
+                    if (cycle % 40 == 0)
+                    {
+                        result += "\n";
+                    }
+
+                    cycle++;
+
+                    if (Math.Abs(((cycle - 1) % 40) - xValue) <= 1)
+                    {
+                        result += "#";
+                    }
+                    else
+                    {
+                        result += ".";
+                    }
+
+                    if (cycle % 40 == 0)
+                    {
+                        result += "\n";
+                    }
+
                     xValue += change;
                 }
             }
