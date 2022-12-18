4e2ce6d77b66253f634cb7b6d6316fae4bb3ab09
Fredrik Andersson <git@git.glyph.se>
2022-12-18 07:05:35 +0100

    Solution to day 18

--- Year2022/Day18/Solver.cs
+++ Year2022/Day18/Solver.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.Year2022.Day18
+﻿using AdventOfCode.Common;
+
+namespace AdventOfCode.Year2022.Day18
 {
     internal class Solver : ISolver
     {
@@ -8,7 +10,44 @@
 
             int result = 0;
 
+            bool[,,] grid = new bool[21, 21, 21];
 
+            foreach (var line in input.AsLines())
+            {
+                var split = line.Split(',').Select(s => int.Parse(s));
+
+                grid[split.ElementAt(0), split.ElementAt(1), split.ElementAt(2)] = true;
+            }
+
+            for (int x = 0; x <= 19; x++)
+            {
+                for (int y = 0; y <= 19; y++)
+                {
+                    for (int z = 0; z <= 19; z++)
+                    {
+                        if (!grid[x, y, z])
+                        {
+                            // no cube here
+                            continue;
+                        }
+
+                        (int, int, int)[] dirs = { (0, 0, 1), (0, 1, 0), (1, 0, 0), (0, 0, -1), (0, -1, 0), (-1, 0, 0) };
+
+                        foreach ((int xDiff, int yDiff, int zDiff) in dirs)
+                        {
+                            try
+                            {
+                                if (!grid[x + xDiff, y + yDiff, z + zDiff])
+                                    result++;
+                            }
+                            catch (IndexOutOfRangeException)
+                            {
+                                result++;
+                            }
+                        }
+                    }
+                }
+            }
 
             return result.ToString();
         }
@@ -19,9 +58,96 @@
 
             int result = 0;
 
+            bool?[,,] grid = new bool?[21, 21, 21];
 
+            foreach (var line in input.AsLines())
+            {
+                var split = line.Split(',').Select(s => int.Parse(s));
+
+                grid[split.ElementAt(0), split.ElementAt(1), split.ElementAt(2)] = true;
+            }
+
+
+            Point start = new Point() { x = 0, y = 0, z = 0 };
+
+            grid[start.x, start.y, start.z] = false;
+
+            Queue<Point> expansion = new();
+            expansion.Enqueue(start);
+
+            List<Point> airCubes = new();
+
+            bool[,,] airGrid = new bool[21, 21, 21];
+            while (expansion.Count != 0)
+            {
+                Point p = expansion.Dequeue();
+
+                if (p.x < 0 || p.x > 21 || p.y < 0 || p.y > 21 || p.z < 0 || p.z > 21)
+                {
+                    continue;
+                }
+
+                (int, int, int)[] dirs = { (0, 0, 1), (0, 1, 0), (1, 0, 0), (0, 0, -1), (0, -1, 0), (-1, 0, 0) };
+
+                foreach ((int xDiff, int yDiff, int zDiff) in dirs)
+                {
+                    try
+                    {
+                        if (grid[p.x + xDiff, p.y + yDiff, p.z + zDiff] == null)
+                        {
+                            // null means nothing now
+                            Point next = new Point() { x = p.x + xDiff, y = p.y + yDiff, z = p.z + zDiff };
+
+                            // false means air
+                            grid[p.x + xDiff, p.y + yDiff, p.z + zDiff] = false;
+
+                            expansion.Enqueue(next);
+                        }
+                    }
+                    catch (IndexOutOfRangeException) { }
+                }
+            }
+
+            for (int x = 0; x <= 19; x++)
+            {
+                for (int y = 0; y <= 19; y++)
+                {
+                    for (int z = 0; z <= 19; z++)
+                    {
+                        bool? cube = grid[x, y, z];
+
+                        if (cube != true)
+                        {
+                            // no cube here
+                            continue;
+                        }
+
+                        (int, int, int)[] dirs = { (0, 0, 1), (0, 1, 0), (1, 0, 0), (0, 0, -1), (0, -1, 0), (-1, 0, 0) };
+
+                        foreach ((int xDiff, int yDiff, int zDiff) in dirs)
+                        {
+                            try
+                            {
+                                if (grid[x + xDiff, y + yDiff, z + zDiff] == false)
+                                    result++;
+                            }
+                            catch (IndexOutOfRangeException)
+                            {
+                                result++;
+                            }
+                        }
+                    }
+                }
+            }
 
             return result.ToString();
+        }
+
+        public class Point
+        {
+            public int x;
+            public int y;
+            public int z;
         }
     }
 }
