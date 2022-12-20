c6016481a8af9cd2eebb81427c270145a9f5eb3c
Fredrik Andersson <git@git.glyph.se>
2022-12-20 07:11:13 +0100

    Prepare day 21

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     internal class Program
     {
         static readonly int YEAR = 2022;
-        static readonly int DAY = 20;
+        static readonly int DAY = 21;
 
         public static async Task Main(string[] args)
         {
