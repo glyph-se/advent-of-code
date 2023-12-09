d7991adf0ab467a32853c6699b833660b0201df2
Fredrik Andersson <git@git.glyph.se>
2023-12-09 06:53:49 +0100

    Day 9 with data

--- Year2023/Day09/Solver.cs
+++ Year2023/Day09/Solver.cs
@@ -13,21 +13,85 @@ public class Solver : ISolver
 
 		foreach (string line in input.AsLines())
 		{
+			IList<long> numbers = line.TrimSplit(" ").Select(l => l.ToLong()).ToList();
 
+			List<long> lastNumbers = new List<long>();
+
+			var diff = numbers;
+
+			while(!diff.All(d => d == 0))
+			{
+				lastNumbers.Add(diff.Last());
+				diff = CalcDiff(diff);
+			}
+
+			lastNumbers.Reverse();
+
+			long nextValue = 0;
+			foreach (long lastNumber in lastNumbers)
+			{
+				nextValue += lastNumber;
+			}
+
+			result += nextValue;
 		}
 
 		return result.ToString();
+	}
+
+	private IList<long> CalcDiff(IList<long> numbers)
+	{
+		var diff = new List<long>();
+
+		for(int i = 1; i<numbers.Count; i++)
+		{
+			diff.Add(numbers[i] - numbers[i-1]);
+		}
+
+		return diff;
+	}
+
+	private IList<long> CalcDiffTwo(IList<long> numbers)
+	{
+		var diff = new List<long>();
+
+		for (int i = 1; i < numbers.Count; i++)
+		{
+			diff.Add(numbers[i-1] - numbers[i]);
+		}
+
+		return diff;
 	}
 
 	public async Task<string> PartTwo(string input)
 	{
 		await Task.Yield();
 
-		int result = 0;
+		long result = 0;
 
 		foreach (string line in input.AsLines())
 		{
+			IList<long> numbers = line.TrimSplit(" ").Select(l => l.ToLong()).ToList();
 
+			List<long> firstNumbers = new List<long>();
+
+			var diff = numbers;
+
+			while (!diff.All(d => d == 0))
+			{
+				firstNumbers.Add(diff.First());
+				diff = CalcDiff(diff);
+			}
+
+			firstNumbers.Reverse();
+
+			long nextValue = 0;
+			foreach (long firstNumber in firstNumbers)
+			{
+				nextValue = firstNumber - nextValue;
+			}
+
+			result += nextValue;
 		}
 
 		return result.ToString();
