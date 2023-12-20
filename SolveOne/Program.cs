d314c8e05b2d6a1a5ed61279ee719088c5a7b91c
Fredrik Andersson <git@git.glyph.se>
2023-12-20 05:56:32 +0100

    Prepare day 20

--- SolveOne/Program.cs
+++ SolveOne/Program.cs
@@ -6,7 +6,7 @@ namespace SolveOne;
 internal class Program
 {
 	static readonly int YEAR = 2023;
-	static readonly int DAY = 19;
+	static readonly int DAY = 20;
 
 	public static async Task Main(string[] args)
 	{
