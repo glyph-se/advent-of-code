2edca6d74b74c5d9eb13daf5aa01fc12de8d3fd4
Fredrik Andersson <git@git.glyph.se>
2023-12-22 08:00:25 +0100

    Day 22

---
+++ Shared/Helpers/Point3D.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace Shared.Helpers
+{
+	[DebuggerDisplay("x={x},y={y},,z={z}")]
+	public class Point3D
+	{
+		public Point3D()
+		{
+
+		}
+
+		public Point3D(int x, int y, int z)
+		{
+			this.x = x;
+			this.y = y;
+		this.z = z;
+	}
+
+		public int x { get; set; }
+		public int y { get; set; }
+		public int z { get; set; }
+
+		public override bool Equals(object? obj)
+		{
+			return obj is Point3D point &&
+					 x == point.x &&
+					 y == point.y &&
+					 z == point.z;
+
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(x, y, z);
+		}
+	}
+}
--- Year2023/Day22/Solver.cs
+++ Year2023/Day22/Solver.cs
@@ -1,5 +1,9 @@
-﻿using Shared;
+﻿using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using Shared;
 using Shared.Helpers;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static Year2023.Day22.Solver;
 
 namespace Year2023.Day22;
 
@@ -11,19 +15,90 @@ public class Solver : ISolver
 
 		long result = 0;
 
-		/*
-		var grid = input.AsGridMatrix((c, x, y) => new CharPoint(c, x, y));
-		grid = grid.ExtendGridMatrix(1, (x, y) => new CharPoint('.', x, y));
-		*/
+		List<Brick> bricks = ParseBricks(input);
 
-		/*
-		foreach (string line in input.AsLines())
+		bricks = bricks
+			.OrderBy(b => Math.Min(b.p1.z, b.p2.z))
+			.ToList();
+
+		int fell = Fall(bricks);
+
+		// Now they are all resting
+
+		foreach (Brick b in bricks)
 		{
+			// Deep copy
+			var without = bricks
+				.Select(b => new Brick(new Point3D(b.p1.x, b.p1.y, b.p1.z), new Point3D(b.p2.x, b.p2.y, b.p2.z)))
+				.ToList();
 
+			// Remove our brick and see how many falls.
+			without.Remove(b);
+
+			int count = Fall(without);
+
+			if (count == 0)
+			{
+				result++;
+			}
 		}
-		*/
 
 		return result.ToString();
+	}
+
+	private static List<Brick> ParseBricks(string input)
+	{
+		List<Brick> bricks = new();
+
+		foreach (string line in input.AsLines())
+		{
+			var split = line.Split('~');
+
+			var p1s = split[0].Split(",");
+			var p2s = split[1].Split(",");
+
+			var p1 = new Point3D(p1s[0].ToInt(), p1s[1].ToInt(), p1s[2].ToInt());
+			var p2 = new Point3D(p2s[0].ToInt(), p2s[1].ToInt(), p2s[2].ToInt());
+			bricks.Add(new Brick(p1, p2));
+		}
+
+		return bricks;
+	}
+
+	public int Fall(List<Brick> bricks)
+	{
+		HashSet<Point3D> occupied = new();
+
+		int count = 0;
+
+		foreach (Brick b in bricks)
+		{
+			int lowestZ = b.Cells().Min(p => p.z);
+			bool did_move = false;
+
+			for (int i = lowestZ; i >= 0; i--)
+			{
+				b.p1.z--;
+				b.p2.z--;
+
+				if (b.Cells().Any(p => occupied.Contains(p)) || b.Cells().Any(p => p.z == 0))
+				{
+					b.p1.z++;
+					b.p2.z++;
+					b.Cells().ForEach(p => occupied.Add(p));
+					break;
+				}
+
+				did_move = true;
+			}
+
+			if(did_move )
+			{
+				count++;
+			}
+		}
+
+		return count;
 	}
 
 	public async Task<string> PartTwo(string input)
@@ -32,6 +107,95 @@ public class Solver : ISolver
 
 		long result = 0;
 
+		List<Brick> bricks = ParseBricks(input);
+
+		// Sort
+		bricks = bricks
+			.OrderBy(b => Math.Min(b.p1.z, b.p2.z))
+			.ToList();
+
+		int fell = Fall(bricks);
+
+		// Now they are all resting
+
+		foreach (Brick b in bricks)
+		{
+			// Deep copy
+			var without = bricks
+				.Select(b => new Brick(new Point3D(b.p1.x, b.p1.y, b.p1.z), new Point3D(b.p2.x, b.p2.y, b.p2.z)))
+				.ToList();
+
+			// Remove our brick and see how many falls.
+			without.Remove(b);
+
+			int count = Fall(without);
+
+			result += count;
+		}
+
 		return result.ToString();
+	}
+
+	public class Brick
+	{
+
+		public Point3D p1;
+		public Point3D p2;
+
+		public Brick(Point3D p1, Point3D p2)
+		{
+			this.p1 = p1;
+			this.p2 = p2;
+		}
+
+		public List<Point3D> Cells()
+		{
+			List<Point3D> result = new List<Point3D>();
+			if(p1.x != p2.x)
+			{
+				int min = Math.Min(p1.x, p2.x);
+				int max = Math.Max(p1.x, p2.x);
+				for(int i = min; i<=max; i++)
+				{
+					result.Add(new Point3D(i, p1.y, p1.z));
+				}
+			}
+			else if (p1.y != p2.y)
+			{
+				int min = Math.Min(p1.y, p2.y);
+				int max = Math.Max(p1.y, p2.y);
+				for (int i = min; i <= max; i++)
+				{
+					result.Add(new Point3D(p1.x, i, p1.z));
+				}
+			}
+			else if (p1.z != p2.z)
+			{
+				int min = Math.Min(p1.z, p2.z);
+				int max = Math.Max(p1.z, p2.z);
+				for (int i = min; i <= max; i++)
+				{
+					result.Add(new Point3D(p1.x, p1.y, i));
+				}
+			}
+			else
+			{
+				result.Add(new Point3D(p1.x, p1.y, p2.z));
+			}
+
+			return result;
+		}
+
+		public override bool Equals(object? obj)
+		{
+			return obj is Brick brick &&
+						 EqualityComparer<Point3D>.Default.Equals(p1, brick.p1) &&
+						 EqualityComparer<Point3D>.Default.Equals(p2, brick.p2);
+		}
+
+		public override int GetHashCode()
+		{
+			return HashCode.Combine(p1, p2);
+		}
 	}
 }
