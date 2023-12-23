aee54cf3819d412dfeb764b5922b153f8dea0d00
Fredrik Andersson <git@git.glyph.se>
2023-12-23 07:48:15 +0100

    Day23

--- Year2023/Day23/Solver.cs
+++ Year2023/Day23/Solver.cs
@@ -11,26 +11,173 @@ public class Solver : ISolver
 
 		long result = 0;
 
-		/*
 		var grid = input.AsGridMatrix((c, x, y) => new CharPoint(c, x, y));
-		grid = grid.ExtendGridMatrix(1, (x, y) => new CharPoint('.', x, y));
-		*/
+		grid = grid.ExtendGridMatrix(1, (x, y) => new CharPoint('#', x, y));
 
-		/*
-		foreach (string line in input.AsLines())
-		{
+		var start = grid[2, 1];
+		var end = grid[grid.GetUpperBound(0) - 2, grid.GetUpperBound(1) - 1];
 
-		}
-		*/
+		result = DfsLongestDistancePart1(grid, start, end);
 
 		return result.ToString();
 	}
+
+	public int DfsLongestDistancePart1(CharPoint[,] grid, CharPoint start, CharPoint end)
+	{
+		int longestDistance = 0;
+		Stack<(CharPoint node, HashSet<CharPoint> visited, int distance)> queue = new();
+		queue.Push((start, new HashSet<CharPoint>(), 0));
+
+		while (queue.Any())
+		{
+			var current = queue.Pop();
+			List<(int dx, int dy)> dirs = new();
+
+			if (current.node == end)
+			{
+				longestDistance = Math.Max(longestDistance, current.distance);
+			}
+			if (current.node.c == '.')
+			{
+				dirs.AddRange(GridHelpers.UpDowns());
+			}
+			if (current.node.c == '>')
+			{
+				dirs.Add((1, 0));
+			}
+			if (current.node.c == '<')
+			{
+				dirs.Add((-1, 0));
+			}
+			if (current.node.c == '^')
+			{
+				dirs.Add((0, -1));
+			}
+			if (current.node.c == 'v')
+			{
+				dirs.Add((0, 1));
+			}
+
+			foreach (var dir in dirs)
+			{
+				var next = grid[current.node.x + dir.dx, current.node.y + dir.dy];
+				if (next.c == '#' || current.visited.Contains(next))
+				{
+					continue;
+				}
+				
+				HashSet<CharPoint> newVisited = new HashSet<CharPoint>(current.visited);
+				newVisited.Add(current.node);
+
+				queue.Push((next, newVisited, current.distance + 1));
+			}
+		}
+
+		return longestDistance;
+	}
+
+	public int DfsLongestDistancePart2(CharPoint[,] grid, CharPoint start, CharPoint end)
+	{
+		int longestDistance = 0;
+		Stack<(CharPoint node, HashSet<CharPoint> visited, int distance)> queue = new();
+		queue.Push((start, new HashSet<CharPoint>(), 0));
+
+		while (queue.Any())
+		{
+			var current = queue.Pop();
+			List<(int dx, int dy)> dirs = new();
+
+			if (current.node == end)
+			{
+				longestDistance = Math.Max(longestDistance, current.distance);
+			}
+			if (current.node.c == '.' || current.node.c == '>' || current.node.c == '<' || current.node.c == '^' || current.node.c == 'v')
+			{
+				dirs.AddRange(GridHelpers.UpDowns());
+			}
+
+			foreach (var dir in dirs)
+			{
+				var next = grid[current.node.x + dir.dx, current.node.y + dir.dy];
+				if (next.c == '#' || current.visited.Contains(next))
+				{
+					continue;
+				}
+
+				HashSet<CharPoint> newVisited = new HashSet<CharPoint>(current.visited);
+				newVisited.Add(current.node);
+
+				queue.Push((next, newVisited, current.distance + 1));
+			}
+		}
+
+		return longestDistance;
+	}
+
+	public int DfsLongestDistancePart1Recursive(CharPoint[,] grid, CharPoint current, CharPoint end, HashSet<CharPoint> visited)
+	{
+		List<(int dx, int dy)> dirs = new();
+
+		if (current == end)
+		{
+			return visited.Count;
+		}
+		if (current.c == '.')
+		{
+			dirs.AddRange(GridHelpers.UpDowns());
+		}
+		if (current.c == '>')
+		{
+			dirs.Add((1, 0));
+		}
+		if (current.c == '<')
+		{
+			dirs.Add((-1, 0));
+		}
+		if (current.c == '^')
+		{
+			dirs.Add((0, -1));
+		}
+		if (current.c == 'v')
+		{
+			dirs.Add((0, 1));
+		}
+
+		int longestDistance = 0;
+
+		foreach (var dir in dirs)
+		{
+			var next = grid[current.x + dir.dx, current.y + dir.dy];
+			if (next.c == '#' || visited.Contains(next))
+			{
+				continue;
+			}
+			visited.Add(next);
+
+			int result = DfsLongestDistancePart1Recursive(grid, next, end, visited);
+
+			longestDistance = Math.Max(longestDistance, result);
+
+			visited.Remove(next);
+		}
+
+		return longestDistance;
+	}
+
 
 	public async Task<string> PartTwo(string input)
 	{
 		await Task.Yield();
 
 		long result = 0;
+
+		var grid = input.AsGridMatrix((c, x, y) => new CharPoint(c, x, y));
+		grid = grid.ExtendGridMatrix(1, (x, y) => new CharPoint('#', x, y));
+
+		var start = grid[2, 1];
+		var end = grid[grid.GetUpperBound(0) - 2, grid.GetUpperBound(1) - 1];
+
+		result = DfsLongestDistancePart2(grid, start, end);
 
 		return result.ToString();
 	}
