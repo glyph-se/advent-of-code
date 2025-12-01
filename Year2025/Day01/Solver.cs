6f3a912796cd4bfa66476cbc3b0a6d7ef9b8a14a
Fredrik Andersson <glyph-se@users.noreply.github.com>
2025-12-01 14:47:29 +0100

    Day 1 solved

---
+++ Year2025/Day01/Solver.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using Shared;
+using Shared.Helpers;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Year2025.Day01;
+
+public class Solver : ISolver
+{
+	public async Task<string> PartOne(string input)
+	{
+		await Task.Yield();
+
+		int pos = 50;
+
+		long result = 0;
+
+		foreach (string line in input.AsLines())
+		{
+			string dir = line.Substring(0, 1);
+			int dist = line.Substring(1).ToInt();
+
+			if (dir == "R")
+			{
+				pos += dist;
+			}
+			else if (dir == "L")
+			{
+				pos -= dist;
+			}
+
+			pos = pos % 100;
+
+			if (pos == 0)
+			{
+				result++;
+			}
+		}
+
+		return result.ToString();
+	}
+
+	public async Task<string> PartTwo(string input)
+	{
+		await Task.Yield();
+
+		int pos = 50;
+
+		long result = 0;
+
+		foreach (string line in input.AsLines())
+		{
+			string dir = line.Substring(0, 1);
+			int dist = line.Substring(1).ToInt();
+
+			if (dir == "R")
+			{
+				for (int i = 0; i < dist; i++)
+				{
+					pos++;
+					if (pos == 100)
+					{
+						result++;
+						pos = 0;
+					}
+				}
+			}
+			else if (dir == "L")
+			{
+				for (int i = 0; i < dist; i++)
+				{
+					pos--;
+					if (pos == 0)
+					{
+						result++;
+					}
+					if (pos == -1)
+					{
+						pos = 99;
+					}
+				}
+			}
+		}
+
+		return result.ToString();
+	}
+}
