a52b5bf71afe81f6484221b782ab6070994f1bd8
Fredrik Andersson <glyph-se@users.noreply.github.com>
2025-12-03 22:01:59 +0100

    Prepare day 4

--- SolveOne/Program.cs
+++ SolveOne/Program.cs
@@ -7,7 +7,7 @@ namespace SolveOne;
 internal class Program
 {
 	static readonly int YEAR = 2025;
-	static readonly int DAY = 3;
+	static readonly int DAY = 4;
 
 	public static async Task Main(string[] args)
 	{
