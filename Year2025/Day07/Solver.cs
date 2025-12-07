f1016de3e13e4742ad99da466d1fec08f72ebf29
Fredrik Andersson <glyph-se@users.noreply.github.com>
2025-12-07 21:05:16 +0100

    Day7

--- Year2025/Day07/Solver.cs
+++ Year2025/Day07/Solver.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using System.Collections.Concurrent;
 
 namespace Year2025.Day07;
 
@@ -10,25 +10,35 @@ public class Solver : ISolver
 
 		long result = 0;
 
-		/*
 		var grid = input.AsGridMatrix((c, x, y) => new CharPoint(c, x, y));
-		grid = grid.ExtendGridMatrix(1, (x, y) => new CharPoint('.', x, y));
 
-		for (int y = 1; y < grid.LengthY(); y++)
+		CharPoint start = grid.AsList().Where(g => g.c == 'S').Single();
+
+		HashSet<Point> beams = new();
+		beams.Add(new Point(start.x, start.y));
+
+		while (beams.First().y < grid.LengthY())
 		{
-			for (int x = 1; x < grid.LengthX(); x++)
+			HashSet<Point> newBeams = new();
+
+			foreach (var beam in beams)
 			{
-
+				if (grid[beam.x, beam.y + 1].c == '.')
+				{
+					beam.y++;
+					newBeams.Add(beam);
+				}
+				else if (grid[beam.x, beam.y + 1].c == '^')
+				{
+					beam.y++;
+					newBeams.Add(new Point(beam.x - 1, beam.y));
+					newBeams.Add(new Point(beam.x + 1, beam.y));
+					result++;
+				}
 			}
-		}
-		*/
 
-		/*
-		foreach (string line in input.AsLines())
-		{
-
+			beams = newBeams;
 		}
-		*/
 
 		return result.ToString();
 	}
@@ -38,6 +48,38 @@ public class Solver : ISolver
 		await Task.Yield();
 
 		long result = 0;
+
+		var grid = input.AsGridMatrix((c, x, y) => new CharPoint(c, x, y));
+
+		CharPoint start = grid.AsList().Where(g => g.c == 'S').Single();
+
+		// ConcurrentDictionary just to get the "AddOrUpdate" method
+		ConcurrentDictionary<Point, long> beams = new();
+		beams.TryAdd(new Point(start.x, start.y), 1L);
+
+		while (beams.First().Key.y < grid.LengthY())
+		{
+			ConcurrentDictionary<Point, long> newBeams = new();
+
+			foreach (KeyValuePair<Point, long> beam in beams)
+			{
+				if (grid[beam.Key.x, beam.Key.y + 1].c == '.')
+				{
+					beam.Key.y++;
+					newBeams.AddOrUpdate(beam.Key, p => beam.Value, (p, v) => v + beam.Value);
+				}
+				else if (grid[beam.Key.x, beam.Key.y + 1].c == '^')
+				{
+					beam.Key.y++;
+					newBeams.AddOrUpdate(new Point(beam.Key.x - 1, beam.Key.y), p => beam.Value, (p, v) => v + beam.Value);
+					newBeams.AddOrUpdate(new Point(beam.Key.x + 1, beam.Key.y), p => beam.Value, (p, v) => v + beam.Value);
+				}
+			}
+
+			beams = newBeams;
+		}
+
+		result = beams.Values.Sum();
 
 		return result.ToString();
 	}
