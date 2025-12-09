6d718d4e3db4f28c12dd72b6f493e789d12e3f10
Fredrik Andersson <glyph-se@users.noreply.github.com>
2025-12-09 21:56:42 +0100

    Day9 done

--- Year2025/Day09/Solver.cs
+++ Year2025/Day09/Solver.cs
@@ -1,4 +1,5 @@
-﻿namespace Year2025.Day09;
+﻿
+namespace Year2025.Day09;
 
 public class Solver : ISolver
 {
@@ -8,25 +9,22 @@ public class Solver : ISolver
 
 		long result = 0;
 
-		/*
-		var grid = input.AsGridMatrix((c, x, y) => new CharPoint(c, x, y));
-		grid = grid.ExtendGridMatrix(1, (x, y) => new CharPoint('.', x, y));
+		List<Point> points = new();
 
-		for (int y = 1; y < grid.LengthY(); y++)
-		{
-			for (int x = 1; x < grid.LengthX(); x++)
-			{
-
-			}
-		}
-		*/
-
-		/*
 		foreach (string line in input.AsLines())
 		{
-
+			(string x, string y) = line.Split2(",");
+			points.Add(new Point(x.ToInt(), y.ToInt()));
 		}
-		*/
+
+		foreach (IEnumerable<Point> pair in points.DifferentCombinations(2))
+		{
+			long area = Area(pair.First(), pair.Skip(1).First());
+			if (area > result)
+			{
+				result = area;
+			}
+		}
 
 		return result.ToString();
 	}
@@ -37,6 +35,62 @@ public class Solver : ISolver
 
 		long result = 0;
 
+		List<Point> points = new();
+
+		foreach (string line in input.AsLines())
+		{
+			(string x, string y) = line.Split2(",");
+			points.Add(new Point(x.ToInt(), y.ToInt()));
+		}
+
+		foreach (IEnumerable<Point> pair in points.DifferentCombinations(2))
+		{
+			var p1 = pair.First();
+			var p2 = pair.Skip(1).First();
+
+			long x1 = Math.Max(p1.x, p2.x);
+			long x2 = Math.Min(p1.x, p2.x);
+
+			long y1 = Math.Max(p1.y, p2.y);
+			long y2 = Math.Min(p1.y, p2.y);
+
+			bool isValid = true;
+
+			for (int i = 0; i < points.Count; i++)
+			{
+				var l1 = points[i];
+				var l2 = points[(i + 1) % points.Count];
+
+				if (!(Math.Max(l1.x, l2.x) <= x2 || Math.Min(l1.x, l2.x) >= x1 || Math.Max(l1.y, l2.y) <= y2 || Math.Min(l1.y, l2.y) >= y1))
+				{
+					isValid = false;
+					break;
+				}
+			}
+
+			if (!isValid)
+			{
+				continue;
+			}
+
+			long area = Area(p1, p2);
+			if (area > result)
+			{
+				result = area;
+			}
+		}
+
 		return result.ToString();
+	}
+
+	private long Area(Point p1, Point p2)
+	{
+		long x1 = Math.Max(p1.x, p2.x);
+		long x2 = Math.Min(p1.x, p2.x);
+
+		long y1 = Math.Max(p1.y, p2.y);
+		long y2 = Math.Min(p1.y, p2.y);
+
+		return (x1 - x2 + 1) * (y1 - y2 + 1);
 	}
 }
